cc3d0aab95bf592070ef88292975b04cc9a76ab0
Yegor Levandovskyi <yegor.levandovskyi@interactio.io>
2022-01-19 18:38:49 +0200

    Add dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,48 @@ import {Medications} from "./pages/Medications";
 import {AddPet} from "./pages/AddPet";
 import {AddMedication} from "./pages/AddMedication";
 import {HealthRecords} from "./pages/HealthRecords";
-import { AddLog } from "./pages/AddLog";
+import {AddLog} from "./pages/AddLog";
+import {useState} from "react";
+import {ThemeContext} from "./context/theme";
 
 function App() {
+    const [theme, setTheme] = useState("light");
+    const toggleTheme = () => {
+        if (theme === "light") {
+            setTheme("dark");
+            return;
+        }
+
+        setTheme("light");
+    };
+
     return (
-        <main>
-            <Router>
-                <nav>
-                    <Link to="/pets">Pets</Link>
-                    <Link to="/medications">Medications</Link>
-                </nav>
+        <ThemeContext.Provider value={{theme, setTheme, toggleTheme}}>
+            <main data-theme={theme}>
+                <Router>
+                    <nav>
+                        <div>
+                            <button onClick={toggleTheme}>change theme</button>
+                        </div>
+                        <div>
+                            <Link to="/pets">Pets</Link>
+                            <Link to="/medications">Medications</Link>
+                        </div>
+                    </nav>
 
-                <Routes>
-                    <Route path="/pets" element={<Pets />} />
-                    <Route path="/medications" element={<Medications />} />
-                    <Route path="/add-pet" element={<AddPet />} />
-                    <Route path="/add-medication" element={<AddMedication />} />
-                    <Route path="/health-records/:id" element={<HealthRecords />} />
-                    <Route path="/add-log/:id" element={<AddLog />} />
+                    <Routes>
+                        <Route path="/pets" element={<Pets />} />
+                        <Route path="/medications" element={<Medications />} />
+                        <Route path="/add-pet" element={<AddPet />} />
+                        <Route path="/add-medication" element={<AddMedication />} />
+                        <Route path="/health-records/:id" element={<HealthRecords />} />
+                        <Route path="/add-log/:id" element={<AddLog />} />
 
-                    <Route path="*" element={<Navigate to="/pets" />} />
-                </Routes>
-            </Router>
-        </main>
+                        <Route path="*" element={<Navigate to="/pets" />} />
+                    </Routes>
+                </Router>
+            </main>
+        </ThemeContext.Provider>
     );
 }
 
--- src/components/Pet.jsx
+++ src/components/Pet.jsx
@@ -1,7 +1,10 @@
+import {useContext} from "react";
 import {useNavigate} from "react-router-dom";
+import {ThemeContext} from "../context/theme";
 
 export const Pet = ({pet, onDelete}) => {
     const navigate = useNavigate();
+    const {setTheme} = useContext(ThemeContext);
     const showLogs = () => {
         navigate(`/health-records/${pet.id}`);
     };
@@ -16,6 +19,9 @@ export const Pet = ({pet, onDelete}) => {
             </div>
             <div>
                 <button onClick={showLogs}>Show logs</button>
+            </div>
+            <div>
+                <button onClick={() => setTheme("dark")}>Set theme to dark</button>
             </div>
         </div>
     );
